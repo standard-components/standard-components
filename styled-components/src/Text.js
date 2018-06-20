1530b5833674ea6106f60c66302e8f631f83bf29
John Otander <johnotander@gmail.com>
2018-06-20 08:10:24 -0600

    Add text component with tests

---
+++ styled-components/src/Text.js
@@ -0,0 +1,42 @@
+import React, { createElement as el } from 'react'
+import styled from 'styled-components'
+import {
+  color,
+  fontFamily,
+  fontSize,
+  fontWeight,
+  letterSpacing,
+  lineHeight,
+  textAlign,
+  propTypes
+} from 'styled-system'
+
+const Text = ({ is, ...props }) => {
+  const Component = styled(is)`
+    ${color}
+    ${fontFamily}
+    ${fontSize}
+    ${fontWeight}
+    ${letterSpacing}
+    ${lineHeight}
+    ${textAlign}
+  `
+
+  return el(Component, props)
+}
+
+Text.displayName = 'Text'
+Text.propTypes = {
+  ...propTypes.fontFamily,
+  ...propTypes.fontSize,
+  ...propTypes.fontWeight,
+  ...propTypes.letterSpacing,
+  ...propTypes.lineHeight,
+  ...propTypes.textAlign,
+  ...propTypes.propTypes
+}
+Text.defaultProps = {
+  is: 'p'
+}
+
+export default Text
